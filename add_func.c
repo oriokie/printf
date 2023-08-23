92d629645904c21873caf4d8877787fe2409b464
“Edwin <oriokie@icloud.com>
2023-08-23 05:37:08 +0300

    edited

--- add_func.c
+++ add_func.c
@@ -7,8 +7,8 @@
  */
 void print_integer(int *count, va_list args)
 {
-	unsigned int n = va_arg(args, unsigned int);
-	unsigned int num = n;
+	int n = va_arg(args, int);
+	int num = n;
 	int divisor;
 
 	if (num < 0)
